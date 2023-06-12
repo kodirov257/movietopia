c5577d394a24c785ab2bb865ad541a8e4aa7f6b2
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2023-06-12 23:56:23 +0500

    Added login form

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
