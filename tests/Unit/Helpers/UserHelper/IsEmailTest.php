ba57145fd51766c14f7804988acd4f0cffa7991f
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2023-08-24 23:49:21 +0500

    Changed is email test names

--- tests/Unit/Helpers/UserHelper/IsEmailTest.php
+++ tests/Unit/Helpers/UserHelper/IsEmailTest.php
@@ -7,14 +7,14 @@ use Tests\TestCase;
 
 class IsEmailTest extends TestCase
 {
-    public function test_is_email(): void
+    public function test_given_email_is_email(): void
     {
         $result = UserHelper::isEmail('test@example.com');
 
         self::assertTrue($result);
     }
 
-    public function test_is_not_email(): void
+    public function test_given_email_is_not_email(): void
     {
         $result = UserHelper::isEmail('wrong-email');
 
